f8cb1c40b08b8133067a01434ad188f6b83fe289
Flávio Zavan <flaviozavan@gmail.com>
2014-08-04 13:00:33 -0300

    Make the clickatell class work and be configurable

--- includes/classes/webservice/clickatell.class.php
+++ includes/classes/webservice/clickatell.class.php
@@ -6,34 +6,20 @@
 class clickatell extends WP_SMS {
 		private $wsdl_link = "http://api.clickatell.com/http/sendmsg";
 		public $tariff = "http://api.clickatell.com/http/getbalance";
-		public $unitrial = false;
-		public $unit;
-		public $flash = "enable";
-		public $isflash = false;
-        private $api_id = 3117542;
-		
-		public function __construct() {
-			parent::__construct();
-		}
-		
+
 		public function SendSMS() {
-			
+
 			$to = implode($this->to, ",");
-			
+
 			$sms_text = iconv('cp1251', 'utf-8', $this->msg);
-			
+
 			$POST = array (
-                'user'      => $this->username,
-                'password'  => $this->password,
-				'api_id'	=> $this->api_id,
+				'user'	  => $this->username,
+				'password'  => $this->password,
 				'to'		=> $to,
-				'from'	=> $this->from,
+				'api_id'	=> $this->from,
 				'text'		=> $sms_text
 			);
-
-            if ($this->isflash) {
-                $POST['msg_type'] = 'SMS_FLASH';
-            }
 
 			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -42,32 +28,29 @@ class clickatell extends WP_SMS {
 			curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 			curl_setopt($ch, CURLOPT_URL, "{$this->wsdl_link}");
 			$result = curl_exec($ch);
-			
+
 			if ($result) {
-				$jsonObj = json_decode($result);
-				
-				if(null===$jsonObj) {
-					echo "Invalid JSON";
-				} elseif(!empty($jsonObj->error)) {
-					echo "An error occured: " . $jsonObj->error . "(code: " . $jsonObj->code . ")";
-				} else {
-					echo "SMS message is sent. Message id " . $jsonObj->result[0]->sms_id;
-					
+				if (strstr($result, 'ERR')) {
+					echo $result;
+				}
+				else {
+					echo "SMS message sent. $result";
+
 					$this->InsertToDB($this->from, $this->msg, $this->to);
 					$this->Hook('wp_sms_send', $result);
-					
+
 					return true;
 				}
 			} else {
 				echo "API access error";
 			}
 		}
-		
+
 		public function GetCredit() {
-		
-			$result = file_get_contents("{$this->tariff}?user={$this->username}&password={$this->password}&api_id={$this->api_id}");
-			
-			return str_replace('Credit: ', '', $result);
+			$result = file_get_contents("{$this->tariff}?user={$this->username}"
+					. "&password={$this->password}&api_id={$this->from}");
+
+			return (float) str_replace('Credit: ', '', $result);
 
 		}
 	}
